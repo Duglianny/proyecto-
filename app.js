fd598ed5d108d035a810126a9da9090582260016
Duglianny Olavarria <dugliannydesiree1997@gmail.com>
2023-04-22 03:11:15 -0400

    parte js

--- app.js
+++ app.js
@@ -2,57 +2,104 @@
 
 document.addEventListener('DOMContentLoaded', function() {
 
-  const email = {
+  /*const email = {
       nombre: '',
       apellidos: '',
       correo: '',
       telefono: '',
-      empresa: '',
-      select: '',
+      estimado: '',
       comentario: ''
 
-  }
+  }*/
 
   // Seleccionar los elementos de la interfaz
   const nombre = document.querySelector('#nombre');
   const apellidos = document.querySelector('#Apellidos');
   const correo = document.querySelector('#correo');
   const telefono = document.querySelector('#telefono');
-  const empresa = document.querySelector('#empresa');
-  const select = document.querySelector('#select')
+  const estimado = document.querySelector('#estimado')
   const comentario = document.querySelector('#comentario')
-  const btnSubmit = document.querySelector('#formulario button[type="submit"]');
   const formulario = document.querySelector('#formulario');
 
   // Asignar eventos
-  nombre.addEventListener('input', validar);
-  apellidos.addEventListener('input', validar);
-  correo.addEventListener('input', validar);
-  telefono.addEventListener('input', validar);
-  empresa.addEventListener('input', validar);
-  select.addEventListener('input', validar);
-  comentario.addEventListener('input', validar)
 
   formulario.addEventListener('submit', enviarEmail);
 
+    async  function enviarEmail(e) {
+    e.preventDefault();
 
-  function enviarEmail(e) {
-      e.preventDefault();
+    if(nombre.value === '' || apellidos.value === '' ||  correo.value === '' || telefono.value === '' || estimado.value === '' || comentario.value === '') {
+        return mensajeAlerta('Hubo un error', 'error')
+    }
 
-      setTimeout(() => {
+    const data = {
+        nombre: nombre.value,
+        apellidos: apellidos.value,
+        correo: correo.value,
+        telefono: telefono.value,
+        estimado: estimado.value,
+        comentario: comentario.value,
+    }
 
-          
+    try {
+        await fetch('http://191.96.231.2:10010/mensaje', {
+            method: 'POST',
+            body: JSON.stringify(data)
+        })
 
-          // Crear una alerta
-          const alertaExito = document.createElement('P');
-          alertaExito.classList.add('bg-green-500', 'text-white', 'p-2', 'text-center', 'rounded-lg', 'mt-10', 'font-bold', 'text-sm', 'uppercase');
-          alertaExito.textContent = 'Mensaje enviado correctamente';
+        mensajeAlerta('Enviado Correctamente', 'success')
+    } catch (error) {
+        console.log(error)
+        return mensajeAlerta('Hubo un error de conexion', 'error')
+    }
+}
 
-          formulario.appendChild(alertaExito);
+// Crear una alerta
+    function mensajeAlerta(mensaje, tipo) {
+    const alerta = document.createElement('P');
 
-          setTimeout(() => {
-              alertaExito.remove(); 
-          }, 3000);
-      }, 3000);
-  }
+    alerta.classList.add(
+        'text-white',
+        'p-2',
+        'text-center',
+        'rounded-4',
+        'mt-4',
+        'fw-bold',
+        'text-uppercase'
+    );
+
+    if(tipo === 'error') {
+        alerta.classList.add('bg-danger')
+    } else if (tipo === 'success') {
+        alerta.classList.add('bg-success')
+    }
+
+    alerta.textContent = mensaje;
+
+    formulario.appendChild(alerta);
+
+    setTimeout(() => {
+        alerta.remove();
+    }, 3000);
+}
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 })
